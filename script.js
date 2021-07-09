df370b83137bad8166c0377edf85df2654a86b41
Carlos Barros <carlos_dbs@hotmail.com>
2021-07-09 20:58:15 -0300

    Funcionalidade de limpar a lista

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ btnAdicionaTarefa.addEventListener('click', addTask);
 function addTask(){
   let task = inputTxtTarefa.value;
   const taskItem = document.createElement('li');
+  taskItem.classList.add('list-item');
   taskItem.innerHTML = task;
   taskList.appendChild(taskItem);
   
@@ -28,6 +29,16 @@ function completedItemList(){
   const completedItem = event.target;
   completedItem.classList.toggle('completed');
   });
+}
+
+const btnClearAll = document.getElementById('apaga-tudo');
+btnClearAll.addEventListener('click', clearAll);
+
+function clearAll(){
+  const listItem = document.querySelectorAll('.list-item');
+  for (let index = 0; index < listItem.length; index += 1) {
+    listItem[index].remove('list-item');
+  }  
 }
 
 selectItemList();
