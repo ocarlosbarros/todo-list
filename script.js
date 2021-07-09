f6d8fd36d96a9831b0bf2e888ce4c2bb469755f0
Carlos Barros <carlos_dbs@hotmail.com>
2021-07-09 19:31:59 -0300

    Terminada funcionalidade de itens selecionados

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
 const inputTxtTarefa = document.getElementById('texto-tarefa');
 const btnAdicionaTarefa = document.getElementById('criar-tarefa');
+
 const taskList = document.getElementById('lista-tarefas');
-
 btnAdicionaTarefa.addEventListener('click', addTask);
-
 
 function addTask(){
   let task = inputTxtTarefa.value;
   const taskItem = document.createElement('li');
-  taskItem.classList.add('item-list');
   taskItem.innerHTML = task;
   taskList.appendChild(taskItem);
   
 }
-console.log(taskList);
 
-document.querySelectorAll('.item-list').forEach(item => {
-  console.log('item-no-foreach')
-  item.addEventListener('click', selectItemList);
-});
-
-
-
-
-function selectItemList(event){
-  console.log('item-na-funcao');
-  selectedItem = event.target;
-  console.log(selectedItem);
-  selectedItem.style.backgroundColor = 'rgb(' + 128 + ',' + 128 + ',' + 128 + ')';
+function selectItemList(){
+  taskList.addEventListener('click', function(event){
+  const selectedItem = event.target;
+  let selected = document.querySelectorAll('.selected');
+  console.log(selected);
+  for (let index = 0; index < selected.length; index += 1) {
+    selected[index].classList.remove('selected');
+  }
+  selectedItem.classList.add('selected');
+  });
 }
 
-console.log(taskList);
+function completedItemList(){
+  const listItem = document.querySelectorAll('li');
+  console.log(listItem);
+}
+
+completedItemList();
+
+selectItemList();
+
+
