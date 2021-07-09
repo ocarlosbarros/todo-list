91c5e08ac59eb51faab3bdc0001c0b8d78d50584
Carlos Barros <carlos_dbs@hotmail.com>
2021-07-09 17:58:31 -0300

    Adiciona botoes e seus estilos

--- script.js
+++ script.js
@@ -8,16 +8,26 @@ btnAdicionaTarefa.addEventListener('click', addTask);
 function addTask(){
   let task = inputTxtTarefa.value;
   const taskItem = document.createElement('li');
-  taskItem.innerText = task;
   taskItem.classList.add('item-list');
+  taskItem.innerHTML = task;
   taskList.appendChild(taskItem);
-  task = '';
+  
 }
+console.log(taskList);
 
 document.querySelectorAll('.item-list').forEach(item => {
+  console.log('item-no-foreach')
   item.addEventListener('click', selectItemList);
 });
 
+
+
+
 function selectItemList(event){
-  console.log('Passei');
+  console.log('item-na-funcao');
+  selectedItem = event.target;
+  console.log(selectedItem);
+  selectedItem.style.backgroundColor = 'rgb(' + 128 + ',' + 128 + ',' + 128 + ')';
 }
+
+console.log(taskList);
