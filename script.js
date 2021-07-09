a407cfbe288ef48f56975eedd796ed76b836db62
Carlos Barros <carlos_dbs@hotmail.com>
2021-07-09 16:09:18 -0300

    Adiciona selecao de item

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 const inputTxtTarefa = document.getElementById('texto-tarefa');
 const btnAdicionaTarefa = document.getElementById('criar-tarefa');
+const taskList = document.getElementById('lista-tarefas');
 
-console.log(inputTxtTarefa);
-console.log(btnAdicionaTarefa);
+btnAdicionaTarefa.addEventListener('click', addTask);
+
+
+function addTask(){
+  let task = inputTxtTarefa.value;
+  const taskItem = document.createElement('li');
+  taskItem.innerText = task;
+  taskItem.classList.add('item-list');
+  taskList.appendChild(taskItem);
+  task = '';
+}
+
+document.querySelectorAll('.item-list').forEach(item => {
+  item.addEventListener('click', selectItemList);
+});
+
+function selectItemList(event){
+  console.log('Passei');
+}
