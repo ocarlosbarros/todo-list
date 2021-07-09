73d1f1bd83691996051df825748ae83b0a182a8a
Carlos Barros <carlos_dbs@hotmail.com>
2021-07-09 15:22:14 -0300

    Adiciona script.js selecionando input e botao

---
+++ script.js
@@ -0,0 +1,5 @@
+const inputTxtTarefa = document.getElementById('texto-tarefa');
+const btnAdicionaTarefa = document.getElementById('criar-tarefa');
+
+console.log(inputTxtTarefa);
+console.log(btnAdicionaTarefa);
